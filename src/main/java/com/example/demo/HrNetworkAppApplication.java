65eaae524106ddbb15575c3d303292f163643d91
Matthew Conrad <matthew.conrad@outlook.com>
2022-04-23 04:01:47 -0700

    Add Spring and React

---
+++ src/main/java/com/example/demo/HrNetworkAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrNetworkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrNetworkAppApplication.class, args);
+	}
+
+}
